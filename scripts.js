b40d3f22fa441253b2008b4548c205a41a2ce646
johncreeve <johncreeve@gmail.com>
2016-10-31 19:22:44 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,21 +6,6 @@ window.onload = function welcomeNotice()
 	
 document.getElementById("mainImage").addEventListener("click", pick_click);
 
-//In trying to get this to work, I've tried all of these:
-
-//var x = document.getElementsByName("mainImage");
-//x.addEventListener("click", pic_click);
-
-//...also tried this with the above statement
-//var x = document.getElementsByClass("mainImage");
-
-//also tried this approach, but none worked:
-//document.mainImage onclick = "pick_click()"
-//or
-//element.mainImage onclick = "pick_click()"
-//or
-//id.mainImage onclick = "pick_click()"
-
 
 function pic_click()
 	{
